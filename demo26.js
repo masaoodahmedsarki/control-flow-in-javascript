aace39870cbe7d8eb64bb92118b936ae961d0ab4
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-22 17:49:28 +0500

    switch case statement

---
+++ demo26.js
@@ -0,0 +1,52 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 41
+// if (temperature === 40) {
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+// console.log("executed")
+// < , >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    //console.log(`User power:${power}`)
+    
+}
+//console.log(`User power:${power}`)
+
+// const balance = 1000
+//if(balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less then 500");
+// }
+
+// else if (balance < 750){
+//     console.log("less then 750");
+// }
+ 
+// else if(balance < 900){
+//     console.log("less then 900");
+// }
+
+// else{
+//     console.log("less then 1200");
+// }
+
+const UserLoggedIn = true
+const debitCard = true
+const UserLoggedInfromGoogle = false
+const UserLoggedInfromEmail = true
+
+ if (UserLoggedIn && debitCard && 2==2) {
+    console.log("ALLOW TO BUY COURSE")
+ }
+
+ if (UserLoggedInfromGoogle || UserLoggedInfromEmail) {
+     console.log("User logged in")
+ }
---
+++ switchcase26.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3  // also you can use string value in switch case like this = "march"
+ switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("febuary")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("may")
+        break;
+    case 6:
+        console.log("june")
+        break;
+    case 7:
+        console.log("july")
+        break;
+    case 8:
+        console.log("august")
+        break;
+    case 9:
+        console.log("septumber")
+        break;
+    case 10:
+        console.log("octumber")
+        break;
+    case 11:
+        console.log("november")
+        break;
+    case 12:
+        console.log("december")
+        break;
+
+ 
+    default:
+        console.log("month does not match")
+        break;
+ }
